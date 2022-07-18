0d635b3c3a43f0e93ebcf2463638fe0b910de968
juanmoreno98 <juanmoreno17@gmail.com>
2022-07-18 13:58:31 -0500

    16 ejercicios resueltos

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -1,22 +1,27 @@
 // En estas primeras 6 preguntas, reemplaza `null` por la respuesta
 
 // Crea una variable "string", puede contener lo que quieras:
-const nuevaString = null;
+const nuevaString = "logrado"; 
+
+
 
 // Crea una variable numérica, puede ser cualquier número:
-const nuevoNum = null;
+const nuevoNum = 4
 
 // Crea una variable booleana:
-const nuevoBool = null;
+const nuevoBool = true
+
+
+
 
 // Resuelve el siguiente problema matemático:
-const nuevaResta = 10 - null === 5;
+const nuevaResta = 10 - 5 === 5; 
 
 // Resuelve el siguiente problema matemático:
-const nuevaMultiplicacion = 10 * null === 40 ;
+const nuevaMultiplicacion = 10 * 4 === 40 ;
 
 // Resuelve el siguiente problema matemático:
-const nuevoModulo = 21 % 5 === null;
+const nuevoModulo = 21 % 5 === 1;
 
 
 // En los próximos 22 problemas, deberás completar la función.
@@ -25,45 +30,53 @@ const nuevoModulo = 21 % 5 === null;
 // Pista: "console.log()" NO fucionará.
 // No cambies los nombres de las funciones.
 
-function devolverString(str) {
+function devolverString(nuevo) {
   // "Return" la string provista: str
-  // Tu código:
-  
+  // Tu código: 
+  return nuevo
 }
 
-function suma(x, y) {
+
+
+
+function suma(x, y) { 
   // "x" e "y" son números
   // Suma "x" e "y" juntos y devuelve el valor
   // Tu código:
-  
+  return suma = x + y;
 }
 
 function resta(x, y) {
+    return resta = x - y
   // Resta "y" de "x" y devuelve el valor
   // Tu código:
   
 }
 
 function multiplica(x, y) {
+  return multiplica = x * y;
   // Multiplica "x" por "y" y devuelve el valor
   // Tu código:
   
 }
 
-function divide(x, y) {
+function divide(x, y) {  
+return divide = x / y;
   // Divide "x" entre "y" y devuelve el valor
   // Tu código:
   
 }
 
-function sonIguales(x, y) {
+function sonIguales(x, y) { 
+  if (x == y) {return true}; return false 
   // Devuelve "true" si "x" e "y" son iguales
   // De lo contrario, devuelve "false"
   // Tu código:
   
 }
 
-function tienenMismaLongitud(str1, str2) {
+function tienenMismaLongitud(estudiando, aprender) {
+  if (estudiando > aprender) {return true}; return false
   // Devuelve "true" si las dos strings tienen la misma longitud
   // De lo contrario, devuelve "false"
   // Tu código:
@@ -71,6 +84,8 @@ function tienenMismaLongitud(str1, str2) {
 }
 
 function menosQueNoventa(num) {
+  if (num < 90) return true; 
+  return false;
   // Devuelve "true" si el argumento de la función "num" es menor que noventa
   // De lo contrario, devuelve "false"
   // Tu código:
@@ -78,6 +93,7 @@ function menosQueNoventa(num) {
 }
 
 function mayorQueCincuenta(num) {
+if (num > 50) return true; return false;
   // Devuelve "true" si el argumento de la función "num" es mayor que cincuenta
   // De lo contrario, devuelve "false"
   // Tu código:
@@ -85,17 +101,20 @@ function mayorQueCincuenta(num) {
 }
 
 function obtenerResto(x, y) {
+  return obtenerResto = ( x % y );
   // Obten el resto de la división de "x" entre "y"
   // Tu código:
-  
-}
+};
 
-function esPar(num) {
+function esPar(num) { 
+num = (8 / 2 )
+if ( num == 4) return true; return false
   // Devuelve "true" si "num" es par
   // De lo contrario, devuelve "false"
   // Tu código:
   
-}
+};
+
 
 function esImpar(num) {
   // Devuelve "true" si "num" es impar
